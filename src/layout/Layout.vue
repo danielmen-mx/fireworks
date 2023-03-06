f1fe9566d35b79a5d4a7bc99b83b518503999d92
Daniel Mendez <danielmenc@outlook.com>
2023-03-05 23:01:30 -0600

    add repo

---
+++ src/layout/Layout.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-card class="ma-2 pa-2 elevation-5 text-center">
+    <v-btn><v-icon>mdi-home</v-icon></v-btn>
+    <span class="text-h6">Starting project</span>
+  </v-card>
+</template>
