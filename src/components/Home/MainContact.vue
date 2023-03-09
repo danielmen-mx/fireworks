94aac922641f19df0384bcb2b763a517dbbdb925
Daniel Mendez <danielmenc@outlook.com>
2023-03-08 20:15:09 -0600

    #comment move files

--- src/components/Home/MainContact.vue
+++ src/components/Home/MainContact.vue
@@ -1,10 +1,11 @@
 <template>
   <v-card-text class="text-center mt-6">
-    <p class="text-h4 font-weight-bold">Contrata ahora</p>
+    <p class="text-h4 font-weight-bold text-grey-darken-3">Contrata ahora</p>
     <p class="text-h6">Dudas?</p>
     <v-card-item v-for="(button, i) in buttons">
       <v-btn
         class="rounded-pill elevation-2 ma-1 px-4"
+        :color="button.color"
       >
         <v-icon>{{button.icon}}</v-icon>
         {{button.title}}
@@ -21,20 +22,26 @@ export default {
         {
           title: "Llamar por teléfono",
           icon: "mdi-cellphone",
+          color: "#FFA726",
           action: ""
         },
         {
           title: "Enviar msj a WhatsApp",
           icon: "mdi-whatsapp",
+          color: "#26A69A",
           action: ""
         },
         {
           title: "Enviar e-mail",
           icon: "mdi-email",
+          color: "#2196F3",
           action: ""
         }
       ]
     }
+  },
+  methods: {
+    // add method when click button: contact by phonecall, whatsapp, email
   },
 }
 </script>
