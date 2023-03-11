145a638f4d8000199a37361339a854f78bbbeb59
Daniel Mendez <danielmenc@outlook.com>
2023-03-10 20:02:51 -0600

    #comment add new global component

--- src/main.js
+++ src/main.js
@@ -10,12 +10,20 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+// Vue phone input
+import VuePhoneNumberInput from 'vue-phone-number-input'
+import 'vue-phone-number-input/dist/vue-phone-number-input.css'
+
 const vuetify = createVuetify({
   components,
   directives,
 })
 
 const app = createApp(App)
+
+app.config.compilerOptions.isCustomElement = (tag) => tag.includes('-')
+
+app.component('vue-phone-number-input', VuePhoneNumberInput)
 
 app.use(router)
    .use(vuetify)
