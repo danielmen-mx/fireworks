0145d2b788e563897d11efae2a3c6bd520d6f972
Daniel Mendez <danielmenc@outlook.com>
2023-03-13 15:14:43 -0600

    #comment add new library

--- src/main.js
+++ src/main.js
@@ -14,6 +14,10 @@ import * as directives from 'vuetify/directives'
 import VuePhoneNumberInput from 'vue-phone-number-input'
 import 'vue-phone-number-input/dist/vue-phone-number-input.css'
 
+import mitt from 'mitt'
+
+const emitter = mitt()
+
 const vuetify = createVuetify({
   components,
   directives,
@@ -24,6 +28,8 @@ const app = createApp(App)
 app.config.compilerOptions.isCustomElement = (tag) => tag.includes('-')
 
 app.component('vue-phone-number-input', VuePhoneNumberInput)
+
+app.config.globalProperties.emitter = emitter
 
 app.use(router)
    .use(vuetify)
