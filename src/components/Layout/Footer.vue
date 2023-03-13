4ecf8785a626a897ddffe149ba1f90d7dc1c1815
Daniel Mendez <danielmenc@outlook.com>
2023-03-13 14:26:46 -0600

    #comment add social media links to footer

--- src/components/Layout/Footer.vue
+++ src/components/Layout/Footer.vue
@@ -1,5 +1,26 @@
 <template>
-  <v-footer class="elevation-2" color="white">
+  <div class="d-flex w-100 align-center px-4 pt-2" >
+    <p class="text-subtitle-1">Siguenos en nuestras paginas!</p>
+
+    <v-spacer></v-spacer>
+
+    <v-tooltip location="top"
+      v-for="icon in icons"
+      :key="icon.icon"
+      :text="icon.text"
+    >
+      <template v-slot:activator="{ props }">
+        <v-btn
+          v-bind="props"
+          class="mx-4"
+          variant="plain"
+          size="small"
+          :icon="icon.icon"
+        ></v-btn>
+      </template>
+    </v-tooltip>
+  </div>
+  <v-footer class="elevation-2" color="">
     <v-row justify="center" no-gutters>
       <v-col class="text-center mt-4" cols="12">
         {{ new Date().getFullYear() }} — <strong>Momentos Mágicos</strong>
@@ -17,6 +38,20 @@
         'Services',
         'Blog',
         'Contact Us',
+      ],
+      icons: [
+        {
+          icon: 'mdi-facebook',
+          text: 'Click to redirect'
+        },
+        {
+          icon: 'mdi-twitter',
+          text: 'Proximamente...'
+        },
+        {
+          icon: 'mdi-instagram',
+          text: 'Proximamente...'
+        }
       ],
     }),
   }
