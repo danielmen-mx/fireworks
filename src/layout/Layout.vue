96193507fc5de4e343fe2714d09e5f9b9f3cdd3d
Daniel Mendez <danielmenc@outlook.com>
2023-03-13 15:15:55 -0600

    #comment add dialog in layout

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -3,6 +3,7 @@
     <v-layout>
       <Navigation />
       <v-main >
+        <DialogInfo />
         <div class="pa-1" style="min-height: 86vh">
           <router-view />
         </div>
@@ -14,11 +15,13 @@
 <script>
 import Navigation from '@/components/Layout/Navigation.vue'
 import Footer from '@/components/Layout/Footer.vue'
+import DialogInfo from '@/components/Layout/DialogInfo.vue'
 
 export default {
   components:{
     Navigation,
-    Footer
+    Footer,
+    DialogInfo
   }
 }
 </script>
