4ded1f2618600c052a8063c729d0d265e5a0f0b8
Daniel Mendez <danielmenc@outlook.com>
2023-03-16 01:49:21 -0600

    #comment add tiomestamp to comment in production

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
+# get timestamp
+now=$(date +"%T")
+
 # build
 npm run build
 
 # add dist folder
 git add -A
-git commit -m "deploy production"
+git commit -m "deploy production $now"
 
 # push changes
 git push
