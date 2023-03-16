81c37b95968521f95ca35cc9ed3abd9ff077e656
Daniel Mendez <danielmenc@outlook.com>
2023-03-16 01:28:06 -0600

    #comment improve bash for update production

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,9 @@
 # build
 npm run build
 
-# save the latest commit hash as a string
-LOGSTRING=$(git log)
-COMMIT=$(echo $LOGSTRING | awk '{print $2}')
+# add dist folder
+git add .dist
+git commit -m "deploy production"
 
-# navigate into the build output directory
-cd dist
-
-# if you're deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m "deploy (commit: $COMMIT)"
-
-# if you're using SSH key authentication
-# git push -f git@github.com:<USER-NAME>/<REPO-NAME>.git master:custom-branch
-
-# if you're using HTTPS/2FA authentication
-# git push -f https://github.com/<USER-NAME>/<REPO-NAME>.git master:custom-branch
-git push -f https://github.com/danielmen-mx/fireworks.git master:production-branch
-
-cd ..
+# push changes
+git push
